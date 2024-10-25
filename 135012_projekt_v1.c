04342486f6018c786d5ebb4dd32478d4d6b65581
aaoovava <WasiliyLolita6@gmail.com>
2024-10-25 13:50:21 +0200

    start of second part(n func mb done)

--- 135012_projekt_v1.c
+++ 135012_projekt_v1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define ID_LENGTH 10
 #define POZNAMKA_LENGTH 500
 
@@ -80,30 +81,93 @@ void h(FILE** string) {
     }
 }
 
+void freeArray(char** array, int countOfLines) {
+    int i;
+    if (array != NULL) {
+        for ( i = 0; i < countOfLines; i++) {
+            free(array[i]);
+        }
+        free(array);
+    }
+}
 
-void k(FILE* data, FILE* parse, FILE* string) {
-    if (data != NULL) fclose(data);
+void n(FILE** data, FILE** parse, FILE** string, char*** dataArray, char*** parseArray, char*** stringArray, int* countOfLines) {
+    char temp[POZNAMKA_LENGTH];
+    int i;
+    
+    if (*string == NULL || *data == NULL || *parse == NULL) {
+        printf("N: Neotvoreny subor.\n");
+        return;
+    }
+   
+    if (*dataArray != NULL) freeArray(*dataArray, *countOfLines);
+    if (*parseArray != NULL) freeArray(*parseArray, *countOfLines);
+    if (*stringArray != NULL) freeArray(*stringArray, *countOfLines);
+
+    *countOfLines = 0;
+    rewind(*data);
+    while (fgets(temp, POZNAMKA_LENGTH, *data)) {
+        (*countOfLines)++;
+    }
+
+    
+    *dataArray = (char**)malloc(*countOfLines * sizeof(char*));
+    *parseArray = (char**)malloc(*countOfLines * sizeof(char*));
+    *stringArray = (char**)malloc(*countOfLines * sizeof(char*));
+
+    for (i = 0; i < *countOfLines; i++) {
+        (*dataArray)[i] = (char*)malloc(POZNAMKA_LENGTH * sizeof(char));
+        (*parseArray)[i] = (char*)malloc(POZNAMKA_LENGTH * sizeof(char));
+        (*stringArray)[i] = (char*)malloc(POZNAMKA_LENGTH * sizeof(char));
+    }
+
+    
+    rewind(*data);
+    rewind(*parse);
+    rewind(*string);
+
+    for (i = 0; i < *countOfLines; i++) {
+        fgets((*dataArray)[i], POZNAMKA_LENGTH, *data);
+        fgets((*parseArray)[i], POZNAMKA_LENGTH, *parse);
+        fgets((*stringArray)[i], POZNAMKA_LENGTH, *string);
+    }
+
+}
+
+void k(FILE* data, FILE* parse, FILE* string, char*** dataArray, char*** parseArray, char*** stringArray, int* countOfLines) {
+    if (data != NULL) fclose(data); 
     if (parse != NULL) fclose(parse);
     if (string != NULL) fclose(string);
+
+    if (*dataArray != NULL) freeArray(*dataArray, *countOfLines);
+    if (*parseArray != NULL) freeArray(*parseArray, *countOfLines);
+    if (*stringArray != NULL) freeArray(*stringArray, *countOfLines);
 }
 
 int main() {
     FILE* data = NULL, * parse = NULL, * string = NULL;
     char commandInput;
+    int countOfLines = 0;  
+
+    char** dataArray = NULL;
+    char** parseArray = NULL;
+    char** stringArray = NULL;
 
     while (1) {
-        scanf("%c", &commandInput);
+        scanf("%c", &commandInput); 
 
         switch (commandInput) {
-        case 'h' :
+        case 'h':
             h(&string);
             break;
         case 'v':
             v(&data, &parse, &string);
             break;
         case 'k':
-            k(data, parse, string);
+            k(data, parse, string, &dataArray, &parseArray, &stringArray, &countOfLines);
             return 0;
+        case 'n':
+            n(&data, &parse, &string, &dataArray, &parseArray, &stringArray, &countOfLines);
             break;
         default:
             break;
